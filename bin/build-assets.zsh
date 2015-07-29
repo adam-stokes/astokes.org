b9afbd950431ebe9235f08d784f22acb902c45f9
Adam Stokes <adam.stokes@ubuntu.com>
2015-07-28 22:40:51 -0400

    update site
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bin/build-assets.zsh
+++ bin/build-assets.zsh
@@ -2,7 +2,7 @@
 
 LESSC=./node_modules/.bin/lessc
 UGLIFY=./node_modules/.bin/uglifyjs
-lessItems=(src/static/styles/*.less)
+lessItems=(styles/*.less)
 cssItems=(
     bower_components/bootswatch/paper/bootstrap.min.css
     bower_components/highlightjs/styles/monokai.css
@@ -12,10 +12,11 @@ jsItems=(
     bower_components/bootstrap/dist/js/bootstrap.js
     bower_components/flowtype/flowtype.js
     bower_components/highlightjs/highlight.pack.js
-    src/static/js/main.js
+    js/main.js
 )
 
 echo "Prepping build/"
+rm -rf build
 mkdir -p build/{styles,js,fonts,images}
 
 echo "Compiling css"
@@ -23,8 +24,8 @@ $LESSC $lessItems --clean-css build/styles/main.css
 cat $cssItems > build/styles/main.min.css &
 
 echo "Copying static"
-rsync -az src/static/fonts/* build/fonts/. &
-rsync -az src/static/images/* build/images/. &
+rsync -az fonts/* build/fonts/. &
+rsync -az images/* build/images/. &
 
 echo "Writing javascript files..."
 $UGLIFY $jsItems -o build/js/main.min.js
---
+++ bin/build-posts.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+"use strict";
+var promise = require("bluebird");
+var _ = require("lodash");
+var fs = require("mz/fs");
+var isFile = require("is-file-promise");
+var parse = require("genc-parse");
+var mkdirp = require("mkdirp-promise");
+var toml = promise.promisifyAll(require("toml"));
+var expandHome = require("expand-tilde");
+var jade = require("jade");
+var co = require("co");
+var Log = require("log");
+var log = new Log();
+
+function saveSingle(config, posts){
+    _.each(posts, function (post){
+        var template = jade.compileFile("templates/" + post.template);
+        mkdirp("build/" + post.permalink)
+            .then(function() {
+                return fs.writeFile("build/" + post.permalink + "/index.html", template(_.merge(config, post)));
+            });
+    });
+    return promise.all(posts);
+}
+
+function saveCollection(config, posts, templateFile, dst){
+    return mkdirp("build")
+        .then(function() {
+            var template = jade.compileFile("templates/" + templateFile);
+            return fs.writeFile(dst, template({site: config, posts: posts}));
+        });
+}
+
+co(function*(){
+    if (!(yield isFile("./config.toml"))){
+        throw Error("Unable to find config.toml.");
+    }
+    log.info("Parsing config file");
+    var confFile = yield fs.readFile("./config.toml");
+    var config = yield toml.parse(confFile);
+    log.info("Reading posts");
+    var posts = yield parse(expandHome(config.blogPosts));
+    log.info("Writing individual posts");
+    yield saveSingle(config, posts);
+
+    log.info("Writing index");
+    yield saveCollection(config, posts, "index.jade", "build/index.html");
+    log.info("Writing feed");
+    yield saveCollection(config, posts, "feed.jade", "build/feed.xml");
+    return;
+}).catch(function(err){
+    throw Error(err);
+});
+
+
