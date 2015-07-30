dfbebeed1f1bd0bfef1a048bd921f381f25ac17b
Adam Stokes <adam.stokes@ubuntu.com>
2015-07-30 11:26:31 -0400

    use gulp
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- bin/build-assets.zsh
+++ bin/build-assets.zsh
@@ -1,22 +1,17 @@
 #!/usr/bin/zsh
 
-LESSC=./node_modules/.bin/lessc
 UGLIFY=./node_modules/.bin/uglifyjs
-lessItems=(less/*.less)
 cssItems=(
-    bower_components/bootswatch/united/bootstrap.min.css
     bower_components/highlightjs/styles/monokai.css
     static/css/main.css)
 jsItems=(
     bower_components/jquery/dist/jquery.js
-    bower_components/bootstrap/dist/js/bootstrap.js
     bower_components/flowtype/flowtype.js
     bower_components/highlightjs/highlight.pack.js
     static/js/main.js
 )
 
-echo "Compiling css"
-$LESSC $lessItems --clean-css static/css/main.css
+gulp
 cat $cssItems > static/css/main.min.css &
 
 echo "Writing javascript files..."
