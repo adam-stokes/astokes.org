e1e44a0bd4c841c6820bc28b98fd6d7bdb543c1e
Adam Stokes <adam.stokes@ubuntu.com>
2015-08-28 21:48:48 -0400

    update hapi
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- server.js
+++ server.js
@@ -1,261 +1,66 @@
 "use strict";
-var Hapi = require("hapi");
-var Good = require("good");
-var config = require("./config");
-var server = new Hapi.Server();
-var Yar = require("yar");
-var sprintf = require("sprintf-js")
-  .sprintf;
-var moment = require("moment");
-var Post = require("./models/post");
-var mongoose = require("mongoose");
-mongoose.connect(sprintf("mongodb://%s/%s", config.db.conn, config.db.name));
+import Hapi from "hapi";
+import Config from "getconfig";
+import Good from "good";
+import { sprintf } from "sprintf-js";
+import Mongoose from "mongoose";
+import Jade from "jade";
+import Inert from "inert";
+import Vision from "vision";
+import routes from "./lib/routes";
+
+Mongoose.connect(sprintf("mongodb://%s/%s",
+                         Config.database.conn,
+                         Config.database.name));
+
+let serverConfig = {};
+let server = new Hapi.Server(serverConfig);
 
 server.connection({
-  port: config.port
+    host: Config.host,
+    port: Config.port
 });
 
-var yarOpts = {
-  cookieOptions: {
-    password: config.secret,
-    clearInvalid: true,
-    isSecure: false
-  }
-};
-
-server.register({
-  register: Yar,
-  options: yarOpts
-}, function(err) {
-  if (err) {
-    throw Error(err);
-  }
+let plugins = [];
+plugins.push({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require("good-console"),
+            events: {
+                response: "*",
+                log: "*"
+            }
+        }]
+    }
 });
 
-server.register({
-    register: require("vision")
-}, function(err){
-    if(err){
+plugins.push({
+    register: Vision
+});
+
+plugins.push({
+    register: Inert
+});
+
+server.register(plugins, err => {
+    if (err) {
         throw err;
     }
 });
 
-server.register({
-    register: require("inert")
-}, function(err){
-    if(err){
-        throw err;
-    }
-});
+server.route(routes);
 
 server.views({
-  engines: {
-    jade: require("jade")
-  },
-  relativeTo: __dirname,
-  path: "./templates",
-  context: {
-    site: config.info
-  }
-});
-
-server.route({
-  path: "/favicon.ico",
-  method: "GET",
-  handler: {
-    file: "./favicon.ico"
-  }
-});
-
-server.route({
-  path: "/",
-  method: "GET",
-  handler: function(request, reply) {
-    var currentYear = moment()
-      .startOf("year")
-      .toDate();
-    var ctx = {};
-    Post.findByYear(currentYear)
-      .sort({
-        date: -1
-      })
-      .execAsync()
-      .then(function(posts) {
-        ctx.posts = posts;
-        return;
-      })
-      .then(function() {
-        return Post.getUniqueYears()
-          .then(function(years) {
-            ctx.years = years;
-          });
-      })
-      .then(function() {
-        return reply.view("index", ctx);
-      })
-      .catch(function(error) {
-        if (error) {
-          throw Error(error);
-        }
-      });
-  }
-});
-
-server.route({
-  path: "/blog/{year}/{month}/{day}/{slug}",
-  method: "GET",
-  handler: function(request, reply) {
-    var slug = request.params.slug;
-    Post.findOne({
-        "slug": slug
-      })
-      .execAsync()
-      .then(function(post) {
-        reply.view("post", {
-          post: post
-        });
-      });
-  }
-});
-
-server.route({
-  path: "/feed/{tag}",
-  method: "GET",
-  handler: function(request, reply) {
-    var tag = request.params.tag;
-    Post.findByTag(tag)
-      .sort({
-        date: -1
-      })
-      .execAsync()
-      .then(function(posts) {
-        var response = reply.view("feed", {
-          posts: posts,
-          updated: posts[0].datexml
-        }, {
-          layout: false
-        });
-        response.type("application/xml");
-      })
-      .catch(function(error) {
-        throw Error(error);
-      });
-  }
-});
-
-server.route({
-  path: "/sitemap.xml",
-  method: "GET",
-  handler: function(request, reply) {
-    Post.find()
-      .sort({
-        date: -1
-      })
-      .execAsync()
-      .then(function(posts) {
-        var response = reply.view("sitemap", {
-          posts: posts
-        }, {
-          layout: false
-        });
-        response.type("application/xml");
-      });
-  }
-});
-
-server.route({
-  path: "/search",
-  method: "POST",
-  handler: function(request, reply) {
-    var searchItem = request.payload.searchInput;
-    Post.find({
-        $or: [{
-          title: new RegExp(searchItem, "i")
-        }, {
-          tags: new RegExp(searchItem, "i")
-        }]
-      })
-      .sort({
-        date: -1
-      })
-      .execAsync()
-      .then(function(posts) {
-        reply.view("search", {
-          posts: posts,
-          searchInput: searchItem
-        });
-      })
-      .catch(function(error) {
-        if (error) {
-          throw Error(error);
-        }
-      });
-  }
-});
-
-
-server.route({
-  path: "/archives/{year}",
-  method: "GET",
-  handler: function(request, reply) {
-    var year = moment(request.params.year + "-01-01")
-      .toDate();
-    var ctx = {};
-    Post.findByYear(year)
-      .sort({
-        date: -1
-      })
-      .execAsync()
-      .then(function(posts) {
-        ctx.posts = posts;
-        return;
-      })
-      .then(function() {
-        return Post.getUniqueYears()
-          .then(function(years) {
-            ctx.years = years;
-          });
-      })
-      .then(function() {
-        return reply.view("archives", ctx);
-      })
-      .catch(function(err) {
-        if (err) {
-          throw Error(err);
-        }
-      });
-  }
-});
-
-server.route({
-  path: "/static/{path*}",
-  method: "GET",
-  handler: {
-    directory: {
-      path: "./public",
-      listing: false,
-      index: false
+    engines: {
+        jade: Jade
+    },
+    relativeTo: __dirname,
+    path: "./templates",
+    context: {
+        site: Config.site
     }
-  }
 });
 
-server.register({
-  register: Good,
-  options: {
-    reporters: [{
-      reporter: require("good-console"),
-      events: {
-        response: "*",
-        log: "*"
-      }
-    }]
-  }
-}, function(err) {
-  if (err) {
-    throw err; // something bad happened loading the plugin
-  }
 
-  server.start(function() {
-    server.log("info", "Server running at: " + server.info.uri);
-  });
-});
+export default server;
